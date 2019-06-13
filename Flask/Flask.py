b25da5a9de8be4cf9a9b5d8d53ab42319848080a
Deniyasu <kivelz@hotmail.com>
2019-06-13 11:55:32 +0800

    refactor app route

--- Flask/Flask.py
+++ Flask/Flask.py
@@ -1,24 +1,11 @@
 from flask import Flask, request
+import Flask.handler1
 
 app= Flask(__name__)
 
-@app.route('/')
-def helloworld():
-    return '<h1>hello world</h1>'
-@app.route('/greet/', defaults = {'name': 'nobody'})
-@app.route('/greet/<name>')
-def greet(name):
-        return 'Good Morning, ' + name
+app.add_url_rule('/', view_func=Flask.handler1.helloworld)
+app.add_url_rule('/greet/', defaults={'name': 'nobody'}, view_func=Flask.handler1.greet)
 
-@app.route('/add/<int:x>/<int:y>')
-def add(x,y):
-    return 'Total is  ' + str(x+y)
-
-@app.route('/sayhello', methods = ['GET', 'POST'])
-def say_hello():
-    name = request.args.get('name') or request.args.form.get('name')
-    return 'Hello' + name;
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
